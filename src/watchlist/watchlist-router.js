e46bd1fbb7996773082a7c7fd4660ac171292e80
Alon Cohen <alon@cohencodes.com>
2019-06-20 22:29:18 -0700

    fixed watchlist service and router

--- src/watchlist/watchlist-router.js
+++ src/watchlist/watchlist-router.js
@@ -5,23 +5,37 @@ const requireAuth = require('../middleware/jwt-auth');
 const watchListRouter = express.Router();
 const jsonBodyParser = express.json();
 
-watchListRouter.all(requireAuth).post('/', jsonBodyParser, (req, res, next) => {
-  const { title, image_url, user_id } = req.body;
+watchListRouter
+  .all(requireAuth)
+  .route('/')
+  .post(jsonBodyParser, (req, res, next) => {
+    const { title, image_url, user_id } = req.body;
 
-  const newEntry = {
-    user_id,
-    title,
-    image_url
-  };
+    const newEntry = {
+      user_id,
+      title,
+      image_url
+    };
 
-  console.log('new entry received', newEntry);
+    WatchListService.insertShow(req.app.get('db'), newEntry)
+      .then(show => {
+        console.log('show response', show);
+        return res.status(201).json(WatchListService.serializeShow(show));
+      })
+      .catch(next);
+  });
 
-  WatchListService.insertShow(req.app.get('db'), newEntry)
-    .then(show => {
-      console.log('show response', show);
-      return res.status(201).json(WatchListService.serializeShow(show));
-    })
-    .catch(next);
-});
+watchListRouter
+  .all(requireAuth)
+  .route('/:user_id')
+  .get((req, res, next) => {
+    console.log('req: ', req.params);
+    const { user_id } = req.params;
+    WatchListService.getWatchList(req.app.get('db'), user_id).then(
+      watchList => {
+        return res.status(200).json(watchList);
+      }
+    );
+  });
 
 module.exports = watchListRouter;
--- src/watchlist/watchlist-service.js
+++ src/watchlist/watchlist-service.js
@@ -1,18 +1,28 @@
 const xss = require('xss');
 
 const watchListService = {
-  insertShow(db, newEntry) {
-    return db('anime_watchlists')
-      .insert(newEntry)
-      .returning('*')
-      .then(([show]) => show);
-  },
   serializeShow(show) {
     return {
       user_id: show.user_id,
       title: xss(show.title),
       image_url: xss(show.image_url)
     };
+  },
+  insertShow(db, newEntry) {
+    return db('anime_watchlists')
+      .insert(newEntry)
+      .returning('*')
+      .then(([show]) => show);
+  },
+  deleteShow(db, id) {
+    return db('anime_watchlists')
+      .where({ id })
+      .delete();
+  },
+  getWatchList(db, user_id) {
+    return db('anime_watchlists AS show')
+      .select('*')
+      .where({ user_id });
   }
 };
 
