cdc3709b0f8839905e2fe430314851750ece274b
Alon Cohen <alon@cohencodes.com>
2019-06-26 11:13:03 -0700

    prepared for heroku postgres

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/anime-world',
+  DB_URL:
+    process.env.DATABASE_URL || 'postgresql://postgres@localhost/anime-world',
+  TEST_DB_URL: 'postgresql://postgres@localhost/anime-world-test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY
 };
